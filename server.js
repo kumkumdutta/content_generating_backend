f269bae02558d59fc1ee262263f64ffcd5f737f0
KumkumDutta <112754510+kumkumdutta@users.noreply.github.com>
2023-08-04 23:58:46 +0530

    genrated backend

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/ask', async (req, res) => {
+  try {
+    const keyword = req.query.keyword;
+    const category = req.query.category
+    const response = await axios.post('https://api.openai.com/v1/engines/text-davinci-003/completions', {
+      prompt: `generate ${category} about ${keyword}`,
+      max_tokens: 100,
+      temperature: 0.7,
+      n: 1
+    }, {
+      headers: {
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    const content = response.data.choices[0].text.trim();
+    res.json({ content });
+  } catch (error) {
+    console.error('Error:', error.response.data);
+    res.status(500).json({ error: 'Something went wrong' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
