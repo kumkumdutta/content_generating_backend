aff4e5a826b874c60c5d53e4109f009c2d28c4f4
KumkumDutta <112754510+kumkumdutta@users.noreply.github.com>
2023-08-05 00:14:53 +0530

    bug fixed

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 require('dotenv').config();
-
+const cors = require('cors')
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors())
 app.use(express.json());
 
 app.post('/ask', async (req, res) => {
